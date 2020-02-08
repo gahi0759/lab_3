8ece46ed8641c75c29f4541bfd54e053852a58c0
gahi0759 <gahi0759@colorado.edu>
2020-02-07 21:06:50 -0700

    Progress so far

--- lab3_script.sh
+++ lab3_script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Authors : Garrett Hite
+# Authors : Garrett Hite, Weiqiang Wamg
 # Date: 2/7/2020
 
 #Problem 1 Code:
@@ -9,4 +9,8 @@ echo "Enter file name: "
 read fileName
 echo "Enter RegEx: "
 read regEx
-grep $regEx $fileName
+egrep $regEx $fileName
+egrep -c [0-9]{3}-[0-9]{3}-[0-9]{4} $fileName
+egrep -c "\b@|\bcom$" $fileName
+egrep -c 303-[0-9]{3}-[0-9]{4} $fileName
+grep geocities.com $fileName >> email_results.txt
