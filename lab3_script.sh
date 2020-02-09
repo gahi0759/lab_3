faa495cab0134418201b13e7f2637ea66f4a4b37
gahi0759 <gahi0759@colorado.edu>
2020-02-09 09:05:21 -0700

    Adding all of the files for lab 3

--- lab3_script.sh
+++ lab3_script.sh
@@ -11,6 +11,6 @@ echo "Enter RegEx: "
 read regEx
 egrep $regEx $fileName
 egrep -c [0-9]{3}-[0-9]{3}-[0-9]{4} $fileName
-egrep -c "\b@|\bcom$" $fileName
+egrep -c "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$" $fileName
 egrep -c 303-[0-9]{3}-[0-9]{4} $fileName
 grep geocities.com $fileName >> email_results.txt
